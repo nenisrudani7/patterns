042eaa1ff5107c72f6f5bd5db0a3d28064376721
Nenis Rudani <nrudani449@rku.ac.in>
2024-07-09 17:27:39 +0530

    commited

---
+++ j10.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class j10 {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("enter number:--");
+        int num= reader.nextInt();
+        for(int i=1; i<=num; i++){
+          for(int j=1; j<=num; j++){
+            System.out.print(i*j+"  ");
+         
+          }
+          System.out.println();
+        }
+}
+}
