58436bf00380ad1f865e48603105a5913e613d21
Nenis Rudani <nrudani449@rku.ac.in>
2024-07-16 21:15:59 +0530

    added

--- p16.java
+++ p16.java
@@ -2,8 +2,7 @@ public class p16 {
     public static void main(String args[]){
   for(int i=1; i<=5; i++){
     for(int j=1; j<=5; j++){
-          System.out.print((i+j-1)+" ");
-        
+          System.out.print((i+j-1)+" ");    
     } 
     System.out.println();
   }
