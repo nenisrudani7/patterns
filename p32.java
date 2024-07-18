9b66b7d15f971040ce4c5be7208ac3eee85a59d4
Nenis Rudani <nrudani449@rku.ac.in>
2024-07-18 23:18:51 +0530

    ADDED

---
+++ p32.java
@@ -0,0 +1,16 @@
+public class p32 {
+    public static void main(String a[]){
+        int n=5;
+        int x;
+        for(int i=0; i<=n; i++)
+        {
+            x=i;
+            for(int j=1;j<=n; j++){
+                x+=n;
+                System.out.print((char)(x-n+65)+" ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
