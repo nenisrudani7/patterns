f2539e4e7718f837867ccd6bd395e63fb7356142
Nenis Rudani <nrudani449@rku.ac.in>
2024-07-19 17:36:00 +0530

    added

---
+++ p42.java
@@ -0,0 +1,14 @@
+public class p42{
+    public static void main(String a[]){
+      
+        for(int i=1;i<=5; i++){
+            int k=i;
+            for(int j=1; j<=i; j++){
+                
+                System.out.print(k+" ");
+                k++;
+            }
+            System.out.println();
+            }
+        }
+    }
