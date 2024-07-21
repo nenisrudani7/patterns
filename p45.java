624eae98ebb0203b6650b45d69683248b5e073da
Nenis Rudani <nrudani449@rku.ac.in>
2024-07-21 17:24:30 +0530

    added

---
+++ p45.java
@@ -0,0 +1,14 @@
+public class p45 {
+    public static void main(String a[]){
+        int k=1;
+        int n=5;
+        for(int i=1; i<=n;i++){
+            for(int j=1;j<=i; j++){
+                System.out.print(k+" ");
+                k+=2;
+            }
+            System.out.println();
+        
+    }
+}
+}
